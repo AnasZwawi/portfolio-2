25094e68619e3983dd3d16154add3be500c2acf3
AnasZwawi <anaszouaoui17@gmail.com>
2024-12-13 20:45:56 +0100

    Add testimonial and about sections

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -24,8 +24,8 @@ export const Navbar = () => {
                 href={"/"}
                 className="flex items-center gap-1 group relative transition duration-300 px-3 py-2.5"
               >
-                <House className="w-4 h-4 hidden md:block" />
-                Home
+                <House className="size-5" />
+
                 <span className="absolute inset-x-1 h-px bg-gradient-to-r from-black from-10% via-black/60 to-black/10 to-90% transition duration-300 -bottom-0.5 opacity-0 scale-x-0 group-hover:opacity-100 group-hover:scale-x-100"></span>
                 <span className="overflow-hidden absolute inset-0 transition origin-bottom duration-300 opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100">
                   <span className="absolute inset-x-4 -bottom-2 h-full bg-gradient-to-t from-black/50 to-transparent blur rounded-t-full"></span>
@@ -35,8 +35,8 @@ export const Navbar = () => {
                 href={"#projects"}
                 className="flex items-center gap-1 group relative transition duration-300 px-3 py-2.5"
               >
-                <BriefcaseBusiness className="w-4 h-4 hidden md:block" />
-                Projects
+                <BriefcaseBusiness className="size-5" />
+
                 <span className="absolute inset-x-1 h-px bg-gradient-to-r from-black from-10% via-black/60 to-black/10 to-90% transition duration-300 -bottom-0.5 opacity-0 scale-x-0 group-hover:opacity-100 group-hover:scale-x-100"></span>
                 <span className="overflow-hidden absolute inset-0 transition origin-bottom duration-300 opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100">
                   <span className="absolute inset-x-4 -bottom-2 h-full bg-gradient-to-t from-black/50 to-transparent blur rounded-t-full"></span>
@@ -46,8 +46,7 @@ export const Navbar = () => {
                 href={"/blog"}
                 className="flex items-center gap-1 group relative transition duration-300 px-3 py-2.5"
               >
-                <NotebookPen className="w-4 h-4 hidden md:block" />
-                Blog
+                <NotebookPen className="size-5" />
                 <span className="absolute inset-x-1 h-px bg-gradient-to-r from-black from-10% via-black/60 to-black/10 to-90% transition duration-300 -bottom-0.5 opacity-0 scale-x-0 group-hover:opacity-100 group-hover:scale-x-100"></span>
                 <span className="overflow-hidden absolute inset-0 transition origin-bottom duration-300 opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100">
                   <span className="absolute inset-x-4 -bottom-2 h-full bg-gradient-to-t from-black/50 to-transparent blur rounded-t-full"></span>
@@ -57,8 +56,8 @@ export const Navbar = () => {
                 href="#about"
                 className="flex items-center gap-1 group relative transition duration-300 px-3 py-2.5"
               >
-                <UserRound className="w-4 h-4 hidden md:block" />
-                About
+                <UserRound className="size-5" />
+
                 <span className="absolute inset-x-1 h-px bg-gradient-to-r from-black from-10% via-black/60 to-black/10 to-90% transition duration-300 -bottom-0.5 opacity-0 scale-x-0 group-hover:opacity-100 group-hover:scale-x-100"></span>
                 <span className="overflow-hidden absolute inset-0 transition origin-bottom duration-300 opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100">
                   <span className="absolute inset-x-4 -bottom-2 h-full bg-gradient-to-t from-black/50 to-transparent blur rounded-t-full"></span>
@@ -68,8 +67,8 @@ export const Navbar = () => {
                 className="flex gap-1 items-center bg-black rounded-l-full px-5 text-white group relative transition duration-300 py-3"
                 href="#contact"
               >
-                <Mail className="w-4 h-4 hidden md:block" />
-                Contact
+                <Mail className="size-5" />
+
                 <span className="absolute inset-x-1 h-px bg-gradient-to-r from-black from-10% via-black/60 to-black/10 to-90% transition duration-300 -bottom-0.5 opacity-0 scale-x-0 group-hover:opacity-100 group-hover:scale-x-100"></span>
                 <span className="overflow-hidden absolute inset-0 transition origin-bottom duration-300 opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100">
                   <span className="absolute inset-x-4 -bottom-2 h-full bg-gradient-to-t from-black/50 to-transparent blur rounded-t-full"></span>
